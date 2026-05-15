4180e31e5781bb78a3925d907cad19f06c8b1f9a
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-05-15 11:53:49 +0300

    Update Trending.module.css
    Remove unnecessary styles

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router"
 import Home from "./components/home/Home"
-import Catalog from "./components/catalog/Catalog"
+import Catalog from "./components/catalog/AllMovies"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 
