96e110c8707100eb0a9b03f4ce15efb8baf859cf
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-05-18 10:47:17 +0300

    Update Header, Footer, Home and AllMovies component styles for big screens

--- app/src/components/footer/Footer.tsx
+++ app/src/components/footer/Footer.tsx
@@ -4,27 +4,29 @@ import { Link } from "react-router";
 export default function Footer() {
     return (
         <footer className={styles["footer"]}>
-            <div className={styles["footer-logo"]}>
-                mission<span className={styles["footer-logo-accent"]}>movie</span>
-            </div>
-            <div>
-                <ul className={styles["footer-links"]}>
-                    <li>
-                        <Link to="/about" className={`${styles["nav-link"]} ${styles["footer-nav-link"]}`}>About</Link>
-                    </li>
-                    <li>
-                        <Link to="/blog" className={`${styles["nav-link"]} ${styles["footer-nav-link"]}`}>Blog</Link>
-                    </li>
-                    <li>
-                        <Link to="/press" className={`${styles["nav-link"]} ${styles["footer-nav-link"]}`}>Press</Link>
-                    </li>
-                    <li>
-                        <Link to="/privcacy" className={`${styles["nav-link"]} ${styles["footer-nav-link"]}`}>Privacy</Link>
-                    </li>
-                    <li><Link to="/terms" className={`${styles["nav-link"]} ${styles["footer-nav-link"]}`}>Terms</Link></li>
-                </ul>
-            </div>
-            <div className={styles["footer-copy"]}>© 2026 mm</div>
+            <section className={styles["footer-wrapper"]}>
+                <div className={styles["footer-logo"]}>
+                    mission<span className={styles["footer-logo-accent"]}>movie</span>
+                </div>
+                <div>
+                    <ul className={styles["footer-links"]}>
+                        <li>
+                            <Link to="/about" className={`${styles["nav-link"]} ${styles["footer-nav-link"]}`}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/blog" className={`${styles["nav-link"]} ${styles["footer-nav-link"]}`}>Blog</Link>
+                        </li>
+                        <li>
+                            <Link to="/press" className={`${styles["nav-link"]} ${styles["footer-nav-link"]}`}>Press</Link>
+                        </li>
+                        <li>
+                            <Link to="/privcacy" className={`${styles["nav-link"]} ${styles["footer-nav-link"]}`}>Privacy</Link>
+                        </li>
+                        <li><Link to="/terms" className={`${styles["nav-link"]} ${styles["footer-nav-link"]}`}>Terms</Link></li>
+                    </ul>
+                </div>
+                <div className={styles["footer-copy"]}>© 2026 mm</div>
+            </section>
         </footer>
     )
 }
