966652916d54aaac214b674fe5bb4bccf88a605b
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-05-19 10:35:31 +0300

    Add active page state and styles to pagination
    Introduce active page handling and active-button styling for pagination.

--- app/src/components/pagination/PaginationButton.tsx
+++ app/src/components/pagination/PaginationButton.tsx
@@ -1,9 +1,9 @@
 import styles from "./PaginationButton.module.css";
 
-type PaginationButtonProps = { count: number };
+type PaginationButtonProps = { count: number, onPageChange: (page: number)=> void, page: number };
 
 export default function PaginationButton(props: PaginationButtonProps) {
     return (
-        <div className={styles["page-button"]}>{ props.count }</div>
+        <div className={props.count === props.page ? styles["page-button-active"] : styles["page-button"]} onClick={() => props.onPageChange(props.count)}>{ props.count }</div>
     );
 }
--- app/src/components/pagination/PaginationContainer.tsx
+++ app/src/components/pagination/PaginationContainer.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import PaginationButton from "./PaginationButton";
 import styles from "./PaginationContainer.module.css";
 
 type PaginationContainerProps = { count: number[] }
 
+
 export default function PaginationContainer(props: PaginationContainerProps) {
+    const [activePage, setActivePage] = useState(1);
+
+    function activePageHandler(page: number) {
+        setActivePage(page)
+    }
+
     return (
         <section className={styles["pages-container"]}>
-            {props.count.map((button) => <PaginationButton count={button} />)}
+            {props.count.map((button) => <PaginationButton count={button} onPageChange={activePageHandler} page={activePage} />)}
         </section>
     );
 }
