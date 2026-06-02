31e82895e0b262325adb19c22db46e8d069c8c07
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-06-02 11:34:15 +0300

    Add a carousel to the hero section on the home page

--- app/src/components/home/Home.tsx
+++ app/src/components/home/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ButtonPrimary from "../buttons/ButtonPrimary";
 import Hero from "../hero/Hero";
 import Reviews from "../reviews/Reviews";
@@ -7,10 +8,32 @@ import styles from "./Home.module.css";
 
 const options = ["All", "Action", "Drama", "Sci-Fi", "Comedy", "Horror", "Romance", "Documentary"];
 
+
 export default function Home() {
+    const [heroState, setHeroState] = useState(1);
+    const [moveState, SetMoveState] = useState("next");
+    
+    function nextHeroHandler() {
+        setHeroState((state) => state + 1);
+        SetMoveState("next");
+    };
+
+    function previousHeroHandler() {
+        setHeroState((state) => state - 1);
+        SetMoveState("previous");
+    };
+
     return (
         <div className={styles["home-wrapper"]}>
-            <Hero />
+            <section className={styles["hero-wrapper"]}>
+                <span className={heroState < 3 ? styles["next-slide"] : styles["hide-nexts-previous-button"]} onClick={nextHeroHandler}>{"<"}</span>
+                <span className={heroState > 1 ? styles["previous-slide"] : styles["hide-nexts-previous-button"]} onClick={previousHeroHandler}>{">"}</span>
+                <div className={`${styles["hero-container"]} ${styles[`hero-container-state${heroState}-${moveState}`]}`}>
+                    <Hero />
+                    <Hero />
+                    <Hero />
+                </div>
+            </section>
             <SelectionFilter options={options} />
             <Trending />
             <section className={styles["cta-banner"]}>
@@ -20,7 +43,7 @@ export default function Home() {
                     Write reviews, build lists, discover new films, and connect with other cinephiles who share
                     your taste.
                 </p>
-                <ButtonPrimary text="Create your free account" addStyle="cta-btn--large"/>
+                <ButtonPrimary text="Create your free account" addStyle="cta-btn--large" />
             </section>
             <Reviews />
         </div>
