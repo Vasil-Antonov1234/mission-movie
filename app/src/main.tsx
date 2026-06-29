657530d56dc450773f365ca8b0e776cd782898cc
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-06-29 16:53:46 +0300

    Add ScrollToTop component to open each new page at the top

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import ScrollToTop from './components/scroll-to-top/ScrollToTop.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
+            <ScrollToTop />
             <App />
         </BrowserRouter>
     </StrictMode>,
