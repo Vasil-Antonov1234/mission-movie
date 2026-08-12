344a6b6283476bbbf4d724ff4ba1a1df26e9d108
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-08-12 22:32:40 +0300

    Update App.tsx
    Remove redundant code

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,32 +7,10 @@ import AllReviews from "./components/catalog/AllReviews"
 import MovieDetail from "./components/detail/MovieDetail"
 import Login from "./components/login-register/Login"
 import Register from "./components/login-register/Register"
-// import { useState } from "react"
-// import UserContext from "./contexts/UserContext"
-// import type { User } from "./types/types"
-// import useFetch from "./hooks/useFetch"
 
 function App() {
-    // const[user, setUser] = useState<User>({});
-    // const navigate = useNavigate();
-    // const { request } = useFetch()
-
-    // function loginHandler(user: User) {
-    //     setUser(user);
-    //     localStorage.setItem("auth", JSON.stringify(user));
-    //     navigate("/");
-    // };
-
-    // async function logoutHandler() {
-    //     await request("/users/logout", "GET", { accessToken: user.accessToken });
-    //     setUser({});
-    //     localStorage.removeItem("auth");
-    // };
-
-    // const isAuthenticated = !!user?.accessToken;
 
     return (
-        // <UserContext.Provider value={{user, onLogin: loginHandler, onLogout: logoutHandler, isAuthenticated}}>
         <>
             <Header />
             <Routes>
@@ -45,7 +23,6 @@ function App() {
             </Routes>
             <Footer />
         </>
-        // </UserContext.Provider>
     )
 }
 
