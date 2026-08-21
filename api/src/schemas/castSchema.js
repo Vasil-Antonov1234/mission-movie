11b80f2271d5eaf3969a8675d9c2985a10c35f2f
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-08-21 11:08:39 +0300

    Create castSchema.js
    Validate cast data with ZOD on the API

---
+++ api/src/schemas/castSchema.js
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+export const createCastSchema = z.object({
+    firstName: z.string()
+        .min(1, { error: "First name is required" })
+        .regex(/^[A-Z]{1}[a-zA-Z]+$/, { error: "The first name must start with a capital letter and contain only letters" }),
+    lastName: z.string()
+        .min(1, { error: "First name is required" })
+        .regex(/^[A-Z]{1}[a-zA-Z]+$/, { error: "The first name must start with a capital letter and contain only letters" }),
+    bornDate: z.string()
+        .min(1, { error: "Date of born is required" })
+        .regex(/^(January|^February|^March|^April|^May|^June|^July|^August|^September|^October|^November|^December) \d{2}, \d{4}$/, { error: "Ivalid date format"}),
+    placeOfBorn: z.string()
+        .min(1, { error: "Place of born is required" }),
+    imageUrl: z.httpUrl({ error: "Invalid URL format"})
+})
