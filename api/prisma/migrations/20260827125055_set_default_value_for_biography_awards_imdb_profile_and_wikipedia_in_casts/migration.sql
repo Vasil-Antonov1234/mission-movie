818c716e2580ce0435edb990901190701f716d61
Vasil Antonov <vaskoantonov1234@gmail.com>
2026-08-27 15:53:37 +0300

    Update the database
    Set default value for biography, awards, imdbProfile and wikipedia fields in casts table

---
+++ api/prisma/migrations/20260827125055_set_default_value_for_biography_awards_imdb_profile_and_wikipedia_in_casts/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "casts" ALTER COLUMN "awards" SET DEFAULT '',
+ALTER COLUMN "biography" SET DEFAULT '',
+ALTER COLUMN "imdbProfile" SET DEFAULT '',
+ALTER COLUMN "wikipedia" SET DEFAULT '';
