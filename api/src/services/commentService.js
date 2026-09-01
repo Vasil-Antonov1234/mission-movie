450cdc4578a5e6f7dedf97ad00458ddd9442fdea
Vasil Antonov <vaskoantonov1234@gmail.com>
2026-09-01 23:06:25 +0200

    The API does not allowed author of the movie to write comments about its own movies

--- api/src/services/commentService.js
+++ api/src/services/commentService.js
@@ -1,7 +1,14 @@
 import commentRepository from "../repositories/commentRepository"
+import movieRepository from "../repositories/movieRepository";
 
 export default {
     async create(userId, movieId, content) {
+        const movie = await movieRepository.getById(movieId);
+
+        if (movie.authorId === userId) {
+            throw new Error("You cannot write comments about your own movies!");
+        };
+
         return await commentRepository.create(userId, movieId, content);
     },
 
--- api/src/utils/errorUtil.js
+++ api/src/utils/errorUtil.js
@@ -1,6 +1,8 @@
 import * as z from "zod";
 
 export function getErrorMessage(error) {
+    console.log(error.name)
+    
     switch (error.name) {
         case "ZodError":
             return Object.values(z.flattenError(error).fieldErrors).flat().join(", ") || "Invalid input";
