9f1154fe507e341f473aa753d4f9270eec2792fd
Vasil Antonov <vaskoantonov1234@gmail.com>
2026-09-04 22:57:15 +0200

    Update rateService.js
    Calculated the new values of movie rating and movie total rating.

--- api/src/services/rateService.js
+++ api/src/services/rateService.js
@@ -1,3 +1,4 @@
+import movieRepository from "../repositories/movieRepository";
 import rateRepository from "../repositories/rateRepository"
 
 export default {
@@ -5,6 +6,11 @@ export default {
         const rate = await rateRepository.rateMovie(rating, movieId, userId);
         const rates = await rateRepository.getRatesCount(movieId);
         const totalRates = Number(rates[0].count) + 1;
-        
+
+        const movie = await movieRepository.getById(movieId);
+        const movieRating = movie.rating;
+        const newMovieRating = ((movieRating + rating) / 2).toFixed(1);
+        const newTotalRating = movie.totalRating + rating;
+
     }
 }
