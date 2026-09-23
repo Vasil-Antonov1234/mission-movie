7930fd8201652dc8fed814da40e13f22e6a3cdfd
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-09-23 16:54:40 +0300

    Update authMiddleware.js
    A feature to authenticate whether the user is admin has been created to the API

--- api/src/middlewares/authMiddleware.js
+++ api/src/middlewares/authMiddleware.js
@@ -31,3 +31,12 @@ export function isAuthMiddleware(req, res, next) {
 
     next();
 };
+
+export function isAdmin(req, res, next) {
+    
+    if (req.user.role !== "ADMIN") {
+        return res.status(403).json({error: "Forbidden — admin access required"});
+    };
+
+    next();
+}
